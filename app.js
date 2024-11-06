be90ee5cc53b40ba87795b59ef715f062fddaefd
Daada1 <abdulkadirabdirahman716@gmail.com>
2024-11-06 11:01:08 +0300

    commit

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const PORT = 3000
 
 const server = http.createServer((req, res)=> {
     
+     // If the request URL starts with '/tasks', call the taskRoutesRoutes function to handle it
     
     if (req.url.startsWith('/tasks')){
         taskRoutesRoutes(req, res)
     }else{
+         // If the URL doesn't start with '/tasks', respond with a 404 status and a JSON error message
         res.writeHead(404, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({error: 'Not Found'}));
     }
